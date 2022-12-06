e237cf04bc03e2a11e538ca8fb24c692b00fffd3
Dan <frivolousvision@gmail.com>
2022-12-06 09:29:12 -0500

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,33 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. //Order doesn;t matter up here
 
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Configure the HTTP request pipeline.
-//Middleware Section
+// Middleware Section
 // app.UseHttpsRedirection();
+
 //Order very important below!!
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication(); //Asks do you have a valid token?
+
+app.UseAuthorization(); //What are you allowed to do?
 
 app.MapControllers();
 
