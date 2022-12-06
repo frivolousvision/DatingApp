5e44bda1ed883b4cb2186183f4b948799c78b284
Dan <frivolousvision@gmail.com>
2022-12-06 14:14:18 -0500

    End of section 5

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
         {
             _context = context;
         }
-
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
