89674288d275f8b1315aa58c9e101fb23405ce28
Dan <frivolousvision@gmail.com>
2022-12-13 15:53:11 -0500

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,13 +40,15 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
             };
         }
         [HttpPost("login")] // api/account/register
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .FirstOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -62,7 +64,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,8 +19,8 @@ export class AccountService {
         map((response:User) => {
           const user = response;
           if(user){
-            localStorage.setItem('user', JSON.stringify(user))
-            this.currentUserSource.next(user)
+            console.log(user)
+            this.setCurrentUser(user)
           }
         })
       )
@@ -30,13 +30,13 @@ export class AccountService {
       return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
         map(user => {
           if(user) {
-            localStorage.setItem('user', JSON.stringify(user));
-            this.currentUserSource.next(user);
+            this.setCurrentUser(user)
           }
         })
       )
     }
     setCurrentUser(user:User) {
+      localStorage.setItem('user', JSON.stringify(user));
       this.currentUserSource.next(user)
     }
    logout() {
